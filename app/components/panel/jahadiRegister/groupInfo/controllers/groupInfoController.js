edbd808a6b6ed8a5ab2157e91639bfeb42eeb95b
RezaGhx <54306821+RezaGhx@users.noreply.github.com>
2020-05-10 23:47:35 +0430

    remove unnecessary

--- app/components/panel/jahadiRegister/groupInfo/controllers/groupInfoController.js
+++ app/components/panel/jahadiRegister/groupInfo/controllers/groupInfoController.js
@@ -19,54 +19,6 @@ function groupInfoController(groupInfoServices, upload, state, resource) {
     }
   );
 
-  self.ticket = {};
-  self.ticket.priority = "2";
-
-  self.ticketTypeEnum = [
-    {
-      value: "0",
-      key: "فنی"
-    },
-    {
-      value: "1",
-      key: "مالی"
-    },
-    {
-      value: "2",
-      key: "سایر"
-    }
-  ];
-  self.ticket.ticketType = self.ticketTypeEnum[0].value;
-
-  self.uploadLogo = function (file) {
-    if (file.length > 0) {
-      upload
-        .upload({
-          url: apiFileManagerPost,
-          data: {
-            file: file
-          }
-        })
-        .then(
-          function (response) {
-            self.ticket.attachment = response.data[0].fileName;
-          },
-          function (resp) {
-            self.ticket = {};
-          },
-          function (evt) {
-            var progressPercentage = parseInt((100.0 * evt.loaded) / evt.total);
-            if (progressPercentage < 100) {
-              self.loadingShow = true;
-            } else {
-              self.loadingShow = false;
-            }
-            console.log(progressPercentage);
-          }
-        );
-    }
-  };
-
   self.submit = function (form) {
 
     if (self.jahadi.name &&
