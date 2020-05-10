0882c78be63627d1c833ba11c1d60220ee3e210e
RezaGhx <54306821+RezaGhx@users.noreply.github.com>
2020-05-10 20:01:03 +0430

    server adress updated

--- app/shared/constant.js
+++ app/shared/constant.js
@@ -2,30 +2,30 @@ var protocol = location.protocol;
 
 var projectTitle = "پویش مومنانه | سامانه هدفمندسازی مساعدتهای داوطلبانه جهادی";
 
-gateway = protocol + "//api.htdf-dev.ir";
+gateway = protocol + "//localhost:3000";
 
 var mode = "Dev";
 if (mode === "Dev") {
     loanGuide = "5051bf48-b23d-42a1-b15e-9693644bdc30";
     warrantyGuide = "c9ea7479-0685-49f0-860a-a63bf31fec91";
     client_id = "35b71e0f-4ff0-4238-a610-cfde368909d6";
-    gateway = protocol + "//api.htdf-dev.ir";
+    gateway = protocol + "//localhost:3000";
 }
 if (mode === "Test") {
     loanGuide = "5051bf48-b23d-42a1-b15e-9693644bdc30";
     warrantyGuide = "c9ea7479-0685-49f0-860a-a63bf31fec91";
     client_id = "35b71e0f-4ff0-4238-a610-cfde368909d6";
-    gateway = protocol + "//api.htdf-test.ir";
+    gateway = protocol + "//localhost:3000";
 } else if (mode === "Demo") {
     loanGuide = "5051bf48-b23d-42a1-b15e-9693644bdc30";
     warrantyGuide = "c9ea7479-0685-49f0-860a-a63bf31fec91";
     client_id = "dcf1a718-d925-4c37-963d-fdfb8439217f";
-    gateway = protocol + "//api.htdf-demo.ir";
+    gateway = protocol + "//localhost:3000";
 } else if (mode === "Production") {
     loanGuide = "5051bf48-b23d-42a1-b15e-9693644bdc30";
     warrantyGuide = "c9ea7479-0685-49f0-860a-a63bf31fec91";
     client_id = "dcf1a718-d925-4c37-963d-fdfb8439217f";
-    gateway = protocol + "//api.htdf.ir";
+    gateway = protocol + "//localhost:3000";
 }
 
 apiGetWay = gateway + "/csm/v1";
